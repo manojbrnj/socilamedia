f5384689329c25bddf3a36ceb3759bd676997e8b
manoj barange <manojbrnj111@gmail.com>
2023-07-26 15:03:04 +0530

    sdafdsfas

--- routes/handers/authHandlers.js
+++ routes/handers/authHandlers.js
@@ -25,7 +25,9 @@ const loginUserHandler = async (req, res) => {
   try {
     const user = await users.findOne({email: req.body.email});
     if (!user) {
-      return res.status(400).json({msg: 'user not Found'});
+      return res.status(401).json({
+        errors: [{msg: 'Please check userName and password'}],
+      });
     }
     const pass =
       user.encryptPassword(req.body.password) === user.hashedPassword;
